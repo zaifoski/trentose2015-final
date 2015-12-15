616abbdcb4462ba9c2de581299bb89249de6ff80
zaifoski <sofiachimirri@gmailcom>
2015-12-15 17:29:20 +0100

    code

--- js/app.js
+++ js/app.js
@@ -4,21 +4,29 @@
  */
 
 var SantaModel = {
+    requests: null,
+    current: null,
+    i:0,
   
   /* Initializes the model with a list of requests, 
    * and sets the first one as the current one 
    */
    init : function(list){
+       this.requests = requests;
+       this.current = requests[0];
    },
   
    /* It moves "current" to the next request */
    next : function (){
+       this.i++;
+       this.current = requests[this.i];
    },
   
    /* Returns the current request. 
     * If all requests have been processed (there is no current one), it returns null 
     */
    getCurrentRequest : function () {
+       return this.current;
    },  
     
    /* Packs the given item if it fulfills the current request.       
@@ -26,6 +34,58 @@ var SantaModel = {
     * returns 0 if the given item does not fulfill the request
     */
    pack : function(item) {
+       if(this.getCurrentRequest.answer==item){
+           return 1;
+       } else {
+           return 0;
+       }
    }      
   
 };
+var SantaOctopus = {
+    count: 0,
+    current: null,
+    init: function(){
+        SantaView.listen();
+        SantaModel.init();
+        this.newItem();
+    },
+    processAnswer: function(answer){
+        if(SantaModel.pack(answer)==1){
+            this.count++;
+        }
+        SantaModel.next();
+        this.newItem();
+    },
+    newItem: function(){
+        this.current = SantaModel.getCurrentRequest();
+        if(this.current==null){
+            SantaView.displayResult(this.count);
+        } else {
+            SantaView.display(this.current);
+        }
+    }
+};
+var SantaView = {
+    display: function(request){
+        $(".question").html(request.question);
+        for(var i=0; i<request.options.length; i++){
+            $(".question-items").append("<li class=\"item\" id=\"" +request.options[i]+"\">"+request.options[i]+"</li>");
+        }
+    },
+    listen: function(){
+        $(".question-items").on("click","li",function(){
+            $(".question").html("");
+            $(".question-items").html("");
+            SantaOctopus.processAnswer($(this).attr('id'));
+        });
+    },
+    displayResult: function(count){
+        $(".result").html("Total points: "+count);
+    }
+};
+
+
+$(document).ready(function(){
+    SantaOctopus.init();    
+});
